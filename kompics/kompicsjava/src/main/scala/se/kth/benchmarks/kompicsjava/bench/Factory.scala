bb53e3f2cbc46eca536d6c4c15f78999d8631e2b
adamhass <adamhas@kth.se>
2020-11-24 13:34:20 +0100

    kompicsjava sizedthroughput compiles fine, kompact pre-created message issues

--- kompics/kompicsjava/src/main/scala/se/kth/benchmarks/kompicsjava/bench/Factory.scala
+++ kompics/kompicsjava/src/main/scala/se/kth/benchmarks/kompicsjava/bench/Factory.scala
@@ -12,6 +12,6 @@ object Factory extends BenchmarkFactory {
   override def allPairsShortestPath(): Benchmark = AllPairsShortestPath;
   override def chameneos(): Benchmark = Chameneos;
   override def fibonacci(): Benchmark = Fibonacci;
+  override def sizedThroughput(): DistributedBenchmark = SizedThroughput;
   override def atomicBroadcast(): DistributedBenchmark = ???;
-  override def sizedThroughput(): DistributedBenchmark = NetThroughputPingPong;
 }
